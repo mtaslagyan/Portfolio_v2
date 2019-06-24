e998a308a3daafdff486572ef9122abce9b9f375
Mari Taslagyan <32289706+mtaslagyan@users.noreply.github.com>
2019-06-23 23:22:06 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import Header from './Components/Header/Header'
-import Navbar from './Components/Navbar/Navbar'
-import Home from './Components/Home/Home'
-import HtmlAndCss from './Components/HtmlAndCss/HtmlAndCss'
-import Reactjs from './Components/Reactjs/Reactjs'
-import Resume from './Components/Resume/Resume'
-import Contact from './Components/Contact/Contact'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
+import Analysis from './components/Analysis/Analysis'
+import Clients from './components/Clients/Clients'
+import Sessions from './components/Sessions/Sessions'
+import ClientDetails from './components/Clients/ClientDetails'
+import SessionDetails from './components/Sessions/SessionDetails'
 
-function App() {
-  return (
-    <div className="App">
-        
-        <BrowserRouter>
-        <Header/>
-          <div className="container">
-            <div className="row">
-              <div className="offset-md-1 offset-0 col-md-10 col-12">
-                <Switch>
-                  <Route exact path="/" component={Home}/>
-                  <Route path="/htmlAndCss" component={HtmlAndCss}/>
-                  <Route path="/reactjs" component={Reactjs}/>
-                  <Route path="/resume" component={Resume}/>
-                  <Route path="/contact" component={Contact}/>
-                </Switch>
-              </div>
-              <div className="col-md-1 col-0">
-                <Navbar/>
-              </div>
-            </div>
-          </div>
-        </BrowserRouter>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+        <Navbar/>
+          <Switch>
+            <Route exact path="/" activeStyle={{ color: 'red' }} component={Home}/>
+            <Route path='/analysis' activeStyle={{ color: 'red' }} component={Analysis} />
+            <Route path='/sessions' activeStyle={{ color: 'red' }} component={Sessions}/>
+            <Route path='/clients' activeStyle={{ color: 'red' }} component={Clients}/>
+            <Route path='/client/:id' activeStyle={{ color: 'red' }} component={ClientDetails}/>
+            <Route path='/session/:id' activeStyle={{ color: 'red' }} component={SessionDetails}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
