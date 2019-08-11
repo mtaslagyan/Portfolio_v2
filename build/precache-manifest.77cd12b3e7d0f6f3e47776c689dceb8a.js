ffef4cee62acd2226ae431628fdcd7bf743fc0a4
Mari Taslagyan <32289706+mtaslagyan@users.noreply.github.com>
2019-08-11 00:54:59 -0700

    Add files via upload

---
+++ build/precache-manifest.77cd12b3e7d0f6f3e47776c689dceb8a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4553fdce1b30354af33653ff49b0880",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cdf5db095ec295291a9",
+    "url": "/static/css/main.bd09c020.chunk.css"
+  },
+  {
+    "revision": "314b5d613639a9c0adb6",
+    "url": "/static/js/2.6b097899.chunk.js"
+  },
+  {
+    "revision": "5cdf5db095ec295291a9",
+    "url": "/static/js/main.710c5904.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "964becb7764982e374a26c5e27631bef",
+    "url": "/static/media/Taslagyan_Mari_Resume.964becb7.pdf"
+  },
+  {
+    "revision": "12abea94dff61ea848fed045d833173a",
+    "url": "/static/media/buddyjane.12abea94.png"
+  },
+  {
+    "revision": "43c261b44e66c0920138e25d25a9e7d6",
+    "url": "/static/media/csunfac.43c261b4.png"
+  },
+  {
+    "revision": "aa629b09a5b5cb357e23c34de2b32284",
+    "url": "/static/media/developmt.aa629b09.png"
+  },
+  {
+    "revision": "5d14af8c3f1e5cbe9f390f3e126cca68",
+    "url": "/static/media/itrainuwin.5d14af8c.png"
+  },
+  {
+    "revision": "addbc104163ed8b5ff062660c62ffaca",
+    "url": "/static/media/mari_profile.addbc104.png"
+  },
+  {
+    "revision": "b8bd2fe179d4c55c5012f773319b4c04",
+    "url": "/static/media/profits.b8bd2fe1.png"
+  }
+]);
